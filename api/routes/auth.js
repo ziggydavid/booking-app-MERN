cd25e0714e38b724db78a90ebf8aa1d87217fd83
ziggydavid <amuhdavid@gmail.com>
2022-07-06 02:53:38 +0100

    auth update

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {loginController, registerController} from "../controllers/userController.js";
+import {loginController, registerController} from "../controllers/authController.js";
 
 
 const router = express.Router();
